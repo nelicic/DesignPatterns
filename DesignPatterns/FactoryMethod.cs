a22eee6453eba6c3e09abdbabf788510c860574d
Nelik <112435537+nelicic@users.noreply.github.com>
2022-12-15 20:39:53 +0200

    Добавьте файлы проекта.

---
+++ DesignPatterns/FactoryMethod.cs
@@ -0,0 +1,75 @@
+﻿namespace DesignPatterns.FactoryMethod
+{
+    public class FactoryMethod
+    {
+
+    }
+
+    public abstract class Creator
+    {
+        public abstract IProduct FactoryMethod();
+        public string SomeOperation()
+        {
+            var product = FactoryMethod();
+            var result = "Creator: The same creator's code has just worked with " + product.Operation();
+            return result;
+        }
+    }
+
+    public class ConcreteCreator1 : Creator
+    {
+        public override IProduct FactoryMethod()
+        {
+            return new ConcreteProduct1();
+        }
+    }
+
+    public class ConcreteCreator2 : Creator
+    {
+        public override IProduct FactoryMethod()
+        {
+            return new ConcreteProduct2();
+        }
+    }
+
+    public interface IProduct
+    {
+        string Operation();
+    }
+
+    class ConcreteProduct1 : IProduct
+    {
+        public string Operation()
+        {
+            return "Result of Concrete product 1";
+        }
+    }
+
+    class ConcreteProduct2 : IProduct
+    {
+        public string Operation()
+        {
+            return "Result of Concrete product 2";
+        }
+    }
+
+    class Client
+    { 
+        public void Main()
+        {
+            Console.WriteLine("App: Launched with the ConcreteCreator1.");
+            ClientCode(new ConcreteCreator1());
+
+            Console.WriteLine("");
+
+            Console.WriteLine("App: Launched with the ConcreteCreator2.");
+            ClientCode(new ConcreteCreator2());
+        }
+
+        private void ClientCode(Creator creator)
+        {
+            Console.WriteLine("Client: I'm not aware of the creator's class," +
+                "but it still works.\n" + creator.SomeOperation());
+        }
+    }
+}
---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,3 @@
+﻿using DesignPatterns.FactoryMethod;
+
+new Client().Main();
