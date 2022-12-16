95efcf619b4f9d4ca48c39d55cb06226e13a8980
Nelik <112435537+nelicic@users.noreply.github.com>
2022-12-16 14:07:24 +0200

    bridge

---
+++ DesignPatterns/Bridge.cs
@@ -0,0 +1,75 @@
+﻿namespace DesignPatterns.Bridge
+{
+    public class Bridge
+    { }
+
+    public class Abstraction
+    {
+        protected IImplementation _implementation;
+
+        public Abstraction(IImplementation implementation)
+        {
+            _implementation = implementation;
+        }
+
+        public virtual string Operation()
+        {
+            return "Abstract: Base operation with: \n" + _implementation.OperationImplementation();
+        }
+    }
+
+    public class ExtendedAbstraction : Abstraction
+    {
+        public ExtendedAbstraction(IImplementation implementation) : base(implementation)
+        { }
+
+        public override string Operation()
+        {
+            return "Extended abstraction: Extended operation with:\n" + _implementation.OperationImplementation();
+        }
+    }
+
+
+    public interface IImplementation
+    {
+        string OperationImplementation();
+    }
+
+    class ConcreteImplementationA : IImplementation
+    {
+        public string OperationImplementation()
+        {
+            return "Concrete implementationA: The result in platform A.\n";
+        }
+    }
+
+    class ConcreteImplementationB : IImplementation
+    {
+        public string OperationImplementation()
+        {
+            return "Concrete implementationB: The result in platform B.\n";
+        }
+    }
+
+    public class Client
+    {
+        public void Main()
+        {
+            Abstraction abstraction;
+            abstraction = new Abstraction(new ConcreteImplementationA());
+            ClientCode(abstraction);
+
+            Console.WriteLine();
+
+            abstraction = new ExtendedAbstraction(new ConcreteImplementationB());
+            ClientCode(abstraction);
+
+        }
+
+        public void ClientCode(Abstraction abstraction)
+        {
+            Console.WriteLine(abstraction.Operation());
+        }
+    }
+
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,3 +1,3 @@
-﻿using DesignPatterns.Prototype;
+﻿using DesignPatterns.Bridge;
 
 new Client().Main();
